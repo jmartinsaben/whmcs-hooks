fc9d471f446b7b2daacdb9810446a20fdc78e556
JMS <jmsaben@zazalala.com>
2023-09-29 15:08:20 +0100

    misc

--- cloudstore-menu.php
+++ cloudstore-menu.php
@@ -13,5 +13,25 @@ add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
     if (!is_null($primaryNavbar->getChild('Announcements'))) {
         $primaryNavbar->removeChild('Announcements');
     }
+    if (!is_null($primaryNavbar->getChild('Knowledgebase'))) {
+        $primaryNavbar->removeChild('Knowledgebase');
+    }
+    if (!is_null($primaryNavbar->getChild('Network Status'))) {
+        $primaryNavbar->removeChild('Network Status');
+    }
+    if (!is_null($primaryNavbar->getChild('Contact Us'))) {
+        $primaryNavbar->removeChild('Contact Us');
+    }
+    $primaryNavbar->addChild('NOM DE DOMAINE')
+        ->setUri('/domainchecker.php')
+        ->setOrder(1);
+    $primaryNavbar->addChild('SERVEURS VIRTUELS')
+        ->setUri('/index.php?rp=/store/vps-100-africain')
+        ->setOrder(1);
+
+    $primaryNavbar->addChild('HEBERGEMENT WEB')
+        ->setUri('/index.php?rp=/store/shared-hosting-cpanle')
+        ->setOrder(1);
+
 });
 ?>
