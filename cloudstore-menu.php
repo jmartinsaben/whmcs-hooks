f30f179face415fe8ea3368f66ca84acadb75f29
JMS <jmsaben@zazalala.com>
2023-10-02 09:10:38 +0100

    misc

--- cloudstore-menu.php
+++ cloudstore-menu.php
@@ -56,38 +56,39 @@ add_hook('ClientAreaSecondarySidebar', 1, function(MenuItem $secondarySidebar)
      if (!is_null($secondarySidebar->getChild('Categories'))) {
         $secondarySidebar->removeChild('Categories');
     }
-});
-
-add_hook('ClientAreaPrimarySidebar', 1, function(MenuItem $primarySidebar)
-{
-
-     $primarySidebar->addChild('Commandez un serveur virtuel')
+    if (!is_null($actionSidebar->getChild('Actions'))) {
+        $secondarySidebar->removeChild('Categories');
+    }
+    $actionSidebar->addChild('Commandez un serveur virtuel')
         ->setUri('/index.php?rp=/store/vps-100-africain')
         ->setOrder(50);
 
-     $primarySidebar->addChild('Commandez un hébergement web')
+    $actionSidebar->addChild('Commandez un hébergement web')
         ->setUri('/index.php?rp=/store/shared-hosting-cpanle')
         ->setOrder(51);
 
-     $primarySidebar->addChild('Accédez à un ticket')
+    $actionSidebar->addChild('Accédez à un ticket')
         ->setUri('/supporttickets.php')
         ->setOrder(52);
 
-     $primarySidebar->addChild('Accédez à mon espace client')
+    $actionSidebar->addChild('Accédez à mon espace client')
         ->setUri('/clientarea.php')
         ->setOrder(53);
-     $primarySidebar->addChild('Payer une facture')
+    $actionSidebar->addChild('Payer une facture')
         ->setUri('/clientarea.php?action=invoices')
         ->setOrder(54);
-     $primarySidebar->addChild("Voir l'état du réseau")
+    $actionSidebar->addChild("Voir l'état du réseau")
         ->setUri('/serverstatus.php')
         ->setOrder(55);
-     $primarySidebar->addChild('Demander du support')
+    $actionSidebar->addChild('Demander du support')
         ->setUri('/submitticket.php')
         ->setOrder(56);
-     $primarySidebar->addChild('Nous contacter')
+    $actionSidebar->addChild('Nous contacter')
         ->setUri('/contact.php')
         ->setOrder(57);
+});
+
+
 
 });
 ?>
