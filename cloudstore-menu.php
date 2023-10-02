21dd98902f481b90ad1c1d0cece38293e3b3df41
JMS <jmsaben@zazalala.com>
2023-10-02 09:12:27 +0100

    misc

--- cloudstore-menu.php
+++ cloudstore-menu.php
@@ -56,9 +56,8 @@ add_hook('ClientAreaSecondarySidebar', 1, function(MenuItem $secondarySidebar)
      if (!is_null($secondarySidebar->getChild('Categories'))) {
         $secondarySidebar->removeChild('Categories');
     }
-    if (!is_null($actionSidebar->getChild('Actions'))) {
-        $secondarySidebar->removeChild('Categories');
-    }
+    $actionSidebar->getChild('Actions');
+
     $actionSidebar->addChild('Commandez un serveur virtuel')
         ->setUri('/index.php?rp=/store/vps-100-africain')
         ->setOrder(50);
