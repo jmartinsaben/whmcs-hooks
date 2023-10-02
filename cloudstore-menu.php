dcaf669d206f2af585394f057f807af7b07a0617
JMS <jmsaben@zazalala.com>
2023-10-02 09:24:35 +0100

    misc

--- cloudstore-menu.php
+++ cloudstore-menu.php
@@ -56,38 +56,41 @@ add_hook('ClientAreaSecondarySidebar', 1, function(MenuItem $secondarySidebar)
     if (!is_null($secondarySidebar->getChild('Categories'))) {
         $secondarySidebar->removeChild('Categories');
     }
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Commandez un serveur virtuel')
-        ->setUri('/index.php?rp=/store/vps-100-africain')
-        ->setOrder(50);
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Commandez un hébergement web')
-        ->setUri('/index.php?rp=/store/shared-hosting-cpanle')
-        ->setOrder(51);
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Accédez à un ticket')
-        ->setUri('/supporttickets.php')
-        ->setOrder(52);
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Accédez à mon espace client')
-        ->setUri('/clientarea.php')
-        ->setOrder(53);
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Payer une facture')
-        ->setUri('/clientarea.php?action=invoices')
-        ->setOrder(54);
-    $secondarySidebar->getChild('Actions')
-        ->addChild("Voir l'état du réseau")
-        ->setUri('/serverstatus.php')
-        ->setOrder(55);
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Demander du support')
-        ->setUri('/submitticket.php')
-        ->setOrder(56);
-    $secondarySidebar->getChild('Actions')
-        ->addChild('Nous contacter')
-        ->setUri('/contact.php')
-        ->setOrder(57);
+    if (!is_null($secondarySidebar->getChild('Actions'))) {
+
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Commandez un serveur virtuel')
+            ->setUri('/index.php?rp=/store/vps-100-africain')
+            ->setOrder(50);
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Commandez un hébergement web')
+            ->setUri('/index.php?rp=/store/shared-hosting-cpanle')
+            ->setOrder(51);
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Accédez à un ticket')
+            ->setUri('/supporttickets.php')
+            ->setOrder(52);
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Accédez à mon espace client')
+            ->setUri('/clientarea.php')
+            ->setOrder(53);
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Payer une facture')
+            ->setUri('/clientarea.php?action=invoices')
+            ->setOrder(54);
+        $secondarySidebar->getChild('Actions')
+            ->addChild("Voir l'état du réseau")
+            ->setUri('/serverstatus.php')
+            ->setOrder(55);
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Demander du support')
+            ->setUri('/submitticket.php')
+            ->setOrder(56);
+        $secondarySidebar->getChild('Actions')
+            ->addChild('Nous contacter')
+            ->setUri('/contact.php')
+            ->setOrder(57);
+    }
 });
 
 ?>
